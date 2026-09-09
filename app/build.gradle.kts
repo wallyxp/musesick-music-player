b6b16a436d1619e2997558b03328b9fb46c7c3bc
Debjyoti <debjyotinandi76@gmail.com>
2026-09-10 01:25:40 +0530

    fixed git error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.wally.musesick"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.1"
 
         vectorDrawables {
             useSupportLibrary = true
